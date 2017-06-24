c906237f54294b16e65ec77d1fc2d426baf0e8ba
Jam Risser <jam@jamrizzi.com>
2017-06-24 03:52:01 -0500

    Improved routing

--- app/core/routing.js
+++ app/core/routing.js
@@ -1,75 +1,90 @@
-window.addEventListener('WebComponentsReady', () => {
-  const state = store.getState();
+class Page {
 
-  if (window.location.port === '') {
-    page.base(app.baseUrl.replace(/\/$/, ''));
+  isReady = false;
+
+  ready() {
+    const state = store.getState();
+    if (window.location.port === '') {
+      page.base(app.baseUrl.replace(/\/$/, ''));
+    }
+    page('*', this.middleware.bind(this), (ctx, next) => {
+      next();
+    });
+    page('/', (req) => {
+      this.changeRoute({
+        req: req,
+        type: 'parent',
+        slugs: {
+          parent: 'home'
+        }
+      });
+    });
+    page('/:parent', (req) => {
+      this.changeRoute({
+        req: req,
+        type: 'parent',
+        slugs: req.params
+      });
+    });
+    page('/:parent/:child', (req) => {
+      this.changeRoute({
+        req: req,
+        type: 'child',
+        slugs: req.params
+      });
+    });
+    page('*', (req) => {
+      this.changeRoute({
+        req: req,
+        type: 'parent',
+        slugs: {
+          parent: '404'
+        }
+      });
+    });
+    page({
+      hashbang: true
+    });
   }
 
-  function middleware(ctx, next) {
+  middleware(ctx, next) {
+    if (ctx.canonicalPath.match(/^\/#!\//g)) {
+      page.redirect(ctx.canonicalPath.substr(3, ctx.canonicalPath.length - 1));
+    }
     next();
   }
 
-  function changeRoute(route) {
-    const pages = _.filter(state.pages, (page) => {
-      return page.addToMenu;
-    });
-    let selectedMenuItem = -1;
-    for (let i = 0; pages.length > i; i++) {
-      const page = pages[i];
-      if (route.slugs.parent === page.slug) {
-        selectedMenuItem = i;
-        break;
-      }
-    }
-    store.dispatch({
-      type: CHANGE_ROUTE,
-      payload: route
-    });
+  changeRoute(route) {
+    const state = store.getState();
+    if (this.isReady && state.route && state.route.req
+        && state.route.req.canonicalPath === route.req.canonicalPath) return;
+    const selectedMenuItem = _.findIndex(_.filter(state.pages, {
+      addToMenu: true
+    }), { slug: route.slugs.parent });
     if (state.meta.selectedMenuItem !== selectedMenuItem) {
       store.dispatch({
         type: UPDATE_SELECTED_MENU_ITEM,
         payload: selectedMenuItem
       });
     }
+    if (state.meta.appLoaded && !this.isReady) this.isReady = true;
+    store.dispatch({
+      type: CHANGE_ROUTE,
+      payload: _.assign({}, route, {
+        params: this.getParams(route.req.querystring)
+      })
+    });
   }
 
-  page('*', middleware, (ctx, next) => {
-    next();
-  });
+  getParams(querystring) {
+    const urlSearchParams = new URLSearchParams(querystring);
+    const params = {};
+    for (const key of urlSearchParams.keys()) {
+      if (key !== 'next') params[key] = urlSearchParams.get(key);
+    }
+    return params;
+  }
+}
 
-  page('/', (req) => {
-    changeRoute({
-      type: 'parent',
-      slugs: {
-        parent: 'home'
-      }
-    });
-  });
-
-  page('/:parent', (req) => {
-    changeRoute({
-      type: 'parent',
-      slugs: req.params
-    });
-  });
-
-  page('/:parent/:child', (req) => {
-    changeRoute({
-      type: 'child',
-      slugs: req.params
-    });
-  });
-
-  page('*', (req) => {
-    changeRoute({
-      type: 'parent',
-      slugs: {
-        parent: '404'
-      }
-    });
-  });
-
-  page({
-    hashbang: true
-  });
-});
+const _page = new Page();
+window.addEventListener('WebComponentsReady', _page.ready.bind(_page));
