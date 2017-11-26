c5215c8e7a692aab6f9c5b48ae08694fde4cccad
Jam Risser <jam@jamrizzi.com>
2017-11-26 04:59:09 -0600

    Added title to dockerfile

--- deployment/nginx.sh
+++ deployment/nginx.sh
@@ -2,5 +2,6 @@
 
 /sbin/envstamp /etc/nginx/conf.d/default.conf
 /sbin/envstamp /app/content/settings.json
+/sbin/envstamp /app/index.html
 /usr/sbin/nginx
 /usr/bin/tail -f /var/log/nginx/error.log
